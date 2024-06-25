2208e26ea5bd5a90d1b5b6a73f4ea6ede51d94a1
kennethoyahebholo <oyahebholokennethizosea@gmail.com>
2024-06-25 17:40:20 +0100

    first commit

---
+++ app/ui/dashboard/rightbar/rightbar.jsx
@@ -0,0 +1,53 @@
+import Image from "next/image";
+import styles from "./rightbar.module.css";
+import { MdPlayCircleFilled, MdReadMore } from "react-icons/md";
+
+const RightBar = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.bgContainer}>
+          <Image src="/astronaut.png" alt="" fill className={styles.bg} />
+        </div>
+        <div className={styles.texts}>
+          <span className={styles.notification}>Available Now</span>
+          <h3 className={styles.title}>
+            How to use the new version of the admin dashboard
+          </h3>
+          <span className={styles.subtitle}>Takes 4 minutes to learn</span>
+          <p className={styles.description}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum
+            iusto cupiditate deserunt nostrum.
+          </p>
+          <button className={styles.button}>
+            <MdPlayCircleFilled />
+            Watch
+          </button>
+        </div>
+      </div>
+
+      <div className={styles.item}>
+        <div className={styles.bgContainer}>
+          <Image src="/astronaut.png" alt="" fill className={styles.bg} />
+        </div>
+        <div className={styles.texts}>
+          <span className={styles.notification}>Available Now</span>
+          <h3 className={styles.title}>
+            How to use the new version of the admin dashboard
+          </h3>
+          <span className={styles.subtitle}>Takes 4 minutes to learn</span>
+          <p className={styles.description}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum
+            iusto cupiditate deserunt nostrum.
+          </p>
+          <button className={styles.button}>
+            <MdReadMore />
+            Learn
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RightBar;
