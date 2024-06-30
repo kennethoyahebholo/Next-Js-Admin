26a84da6e92ed7b5b5872a9ff2473bf72ebcfb16
kennethoyahebholo <oyahebholokennethizosea@gmail.com>
2024-06-30 11:23:26 +0100

    WIP

---
+++ app/dashboard/products/add/page.jsx
@@ -0,0 +1,30 @@
+import styles from "@/app/ui/dashboard/users/addUsers/addUsers.module.css";
+
+const AddUserPage = () => {
+  return (
+    <div className={styles.container}>
+      <form action="" className={styles.form}>
+        <input type="text" placeholder="title" name="title" required />
+        <select name="cat" id="cat">
+          <option value="general">Choose a categorie</option>
+          <option value="kitchen">Kitchen</option>
+          <option value="phone">Phone</option>
+          <option value="computer">Computer</option>
+        </select>
+        <input type="number" placeholder="price" name="price" />
+        <input type="number" placeholder="stock" name="stock" />
+        <input type="text" placeholder="color" name="color" />
+        <input type="text" placeholder="size" name="size" />
+        <textarea
+          name="desc"
+          id="desc"
+          rows="16"
+          placeholder="Description"
+        ></textarea>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default AddUserPage;
