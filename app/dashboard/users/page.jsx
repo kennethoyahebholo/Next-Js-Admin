8a62b01673b04d1fdbbeb987085e12f017cab066
kennethoyahebholo <oyahebholokennethizosea@gmail.com>
2024-07-14 00:22:31 +0100

    implemented get users and products using next feat — no api

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -7,10 +7,11 @@ import Pagination from "@/app/ui/dashboard/pagination/pagination";
 
 import styles from "../../ui/dashboard/users/users.module.css";
 
-const UsersPage = async () => {
-  const users = await fetchUsers();
+const UsersPage = async ({ searchParams }) => {
+  const q = searchParams?.q || "";
+  const page = searchParams?.page || 1;
 
-  console.log(users, "users");
+  const { count, users } = await fetchUsers(q, page);
   return (
     <div className={styles.container}>
       <div className={styles.top}>
@@ -31,109 +32,44 @@ const UsersPage = async () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>
-              <div className={styles.user}>
-                <Image
-                  src="/noavatar.png"
-                  alt=""
-                  width={40}
-                  height={40}
-                  className={styles.userImage}
-                />
-                John Doe
-              </div>
-            </td>
-            <td>John@gmail.com</td>
-            <td>13.01.2022</td>
-            <td>Admin</td>
-            <td>active</td>
-            <td>
-              <div className={styles.buttons}>
-                <Link href="/dashboard/users/test">
-                  <button className={`${styles.button} ${styles.view}`}>
-                    View
-                  </button>
-                </Link>
+          {users?.map((user) => (
+            <tr key={user?.id}>
+              <td>
+                <div className={styles.user}>
+                  <Image
+                    src={user?.img || "/noavatar.png"}
+                    alt=""
+                    width={40}
+                    height={40}
+                    className={styles.userImage}
+                  />
+                  {user?.username}
+                </div>
+              </td>
+              <td>{user?.email}</td>
+              <td>{user?.createdAt?.toString().slice(4, 16)}</td>
+              <td>{user?.isAdmin ? "Admin" : "Client"}</td>
+              <td>{user?.isActive ? "active" : "passive"}</td>
+              <td>
+                <div className={styles.buttons}>
+                  <Link href={`/dashboard/users/${user?.id}`}>
+                    <button className={`${styles.button} ${styles.view}`}>
+                      View
+                    </button>
+                  </Link>
 
-                <Link href="/">
-                  <button className={`${styles.button} ${styles.delete}`}>
-                    Delete
-                  </button>
-                </Link>
-              </div>
-            </td>
-          </tr>
-
-          <tr>
-            <td>
-              <div className={styles.user}>
-                <Image
-                  src="/noavatar.png"
-                  alt=""
-                  width={40}
-                  height={40}
-                  className={styles.userImage}
-                />
-                John Doe
-              </div>
-            </td>
-            <td>John@gmail.com</td>
-            <td>13.01.2022</td>
-            <td>Admin</td>
-            <td>active</td>
-            <td>
-              <div className={styles.buttons}>
-                <Link href="/">
-                  <button className={`${styles.button} ${styles.view}`}>
-                    View
-                  </button>
-                </Link>
-
-                <Link href="/">
-                  <button className={`${styles.button} ${styles.delete}`}>
-                    Delete
-                  </button>
-                </Link>
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <div className={styles.user}>
-                <Image
-                  src="/noavatar.png"
-                  alt=""
-                  width={40}
-                  height={40}
-                  className={styles.userImage}
-                />
-                John Doe
-              </div>
-            </td>
-            <td>John@gmail.com</td>
-            <td>13.01.2022</td>
-            <td>Admin</td>
-            <td>active</td>
-            <td>
-              <div className={styles.buttons}>
-                <Link href="/">
-                  <button className={`${styles.button} ${styles.view}`}>
-                    View
-                  </button>
-                </Link>
-
-                <Link href="/">
-                  <button className={`${styles.button} ${styles.delete}`}>
-                    Delete
-                  </button>
-                </Link>
-              </div>
-            </td>
-          </tr>
+                  <Link href="/">
+                    <button className={`${styles.button} ${styles.delete}`}>
+                      Delete
+                    </button>
+                  </Link>
+                </div>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
-      <Pagination />
+      <Pagination count={count} />
     </div>
   );
 };
--- app/lib/utils.js
+++ app/lib/utils.js
@@ -2,13 +2,11 @@ import mongoose from "mongoose";
 
 export const connectToDB = async () => {
   const connection = {};
-  console.log(process.env.MONGO, "mongo");
   try {
-    console.log(connection, "con");
     if (connection.isConnected) return;
     const db = await mongoose.connect(process.env.MONGO);
-    console.log(db, "db");
     connection.isConnected = db.connections[0].readyState;
+    console.log("=> Database connected successfully");
   } catch (err) {
     console.log(err, "herer");
     throw new Error(err);
