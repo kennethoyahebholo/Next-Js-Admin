a5c25c69f2b5de5ae1b7d30bcd039e27cc832539
kennethoyahebholo <oyahebholokennethizosea@gmail.com>
2024-07-20 19:14:10 +0100

    ui clean up

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,24 @@
+import Link from "next/link";
+import styles from "./ui/home/home.module.css";
+
 const Homepage = () => {
   return (
-    <div>Homepage</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.main}>
+        <h1 className={styles.welcomeText}>Welcome to Admin Dashboard</h1>
+        <p className={styles.subText}>
+          Log in to manage your account and access exclusive features.
+        </p>
+        <div className={styles.credentials}>
+          <p>Username: Admin</p>
+          <p>Password: MyAdminPassword12%</p>
+        </div>
+        <Link href="./login" className={styles.btnLink}>
+          Log in
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default Homepage
+export default Homepage;
